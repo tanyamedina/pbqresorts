5a2c795f76dd4ee36b0a953f234c43c90a58c75a
tanyamedina <criaturamaldita@gmail.com>
2023-11-15 19:29:16 -0700

    Subiendo proyecto U4A2

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,7 +7,6 @@ export const Home = () => {
         <span>RESORTS EXCLUSIVOS EN LAS</span>
         <span className="small-gap">Soleadas Costas</span>
         <span>de México</span>
-        
         <span>El lujo del Viejo Mundo en la luminosa Costa del Pacífico en Cabo San Lucas</span>
         <span>Entre a un mundo lleno de momentos inolvidables en PBQ Resorts. Sea para unas románticas vacaciones, una aventura de pesca o una temporada en familia, nuestro resort en Cabo San Lucas será su hogar lejos del hogar. Dispóngase a disfrutar del maravilloso mar color turquesa, las resplandecientes arenas, las albercas frente al mar y los plácidos jardines en el tranquilo ambiente distintivo de PBQ Resorts. Nuestro extraordinario resort cautivará sus sentidos.</span>
         </p>
